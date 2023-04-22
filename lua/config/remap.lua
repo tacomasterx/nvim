37e1c83908eb23384ea6cb5e562d28da0ea4583d
Tomas Carrillo <tacoman8918@gmail.com>
2023-04-22 12:59:51 -0600

    Major changes to remap.lua
    - Fixed Comment.nvim custom mapping
    - Added mappings to recently installed pluggins
    - Added some better interactions with clipboard

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,4 +1,7 @@
 local kmap = vim.keymap
+-- kmap.set({"n", "v"}, "K", "<nop>")
+-- kmap.set({"n", "v"}, "J", "<nop>")
+kmap.set({"n", "v"}, "Q", "<nop>")
 
 -- Resize window using <alt> hjkl keys
 kmap.set("n", "<M-k>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
@@ -26,6 +29,26 @@ kmap.set("n", "<Leader>qw", "<cmd>wq<CR>", { desc = "Save file and exit" })
 kmap.set("v", "<", "<gv")
 kmap.set("v", ">", ">gv")
 
+-- Move selected line / block of text in visual mode
+-- shift + k to move up
+-- kmap.set("n", "K", "ddkP", { desc = "Move current line up"})
+-- -- shift + j to move down
+-- kmap.set("n", "J", "ddp", { desc = "Move current line down"})
+-- shift + k to move up
+kmap.set("x", "K", "<cmd>move '<-2<CR>gv=gv", { desc = "Move selected lines up"})
+-- shift + j to move down
+kmap.set("x", "J", "<cmd>move '>+1<CR>gv=gv", { desc = "Move selected lines down"})
+-- Ctrl + j to append line
+kmap.set("n", "J", "mzJ'z", { desc = "Append line below to current line"})
+--
+-- ThePrimagen paste
+kmap.set("x", "<leader>p", "\"_dP", { desc = "Paste ignoring buffer"})
+-- ThePrimagen executable (Linux only)
+kmap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Makes current file executable"})
+-- asbjornhaland yank 
+kmap.set({"n", "v"}, "<leader>y", "\"+y", { desc = "Yanks to clipboard"})
+kmap.set("n", "<leader>Y", "\"+Y", { desc = "Yanks content after cursor to clipboard"})
+
 -- Alternate way to save
 kmap.set("n", "<Leader>W", "<cmd>w!<CR>", { desc = "Forced save" })
 kmap.set("n", "<Leader>wf", "<cmd>w!<CR>", { desc = "Forced save" })
@@ -51,10 +74,10 @@ kmap.set("n", "<leader>l", "<cmd>:Lazy<cr>", { desc = "Lazy" })
 
 -- telescope
 local builtin = require('telescope.builtin')
-kmap.set('n', '<leader>ff', builtin.find_files, {})
-kmap.set('n', '<leader>fg', builtin.live_grep, {})
-kmap.set('n', '<leader>fb', builtin.buffers, {})
-kmap.set('n', '<leader>fh', builtin.help_tags, {})
+kmap.set('n', '<leader>ff', builtin.find_files, { desc = "Fuzzy find files"})
+kmap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep"})
+kmap.set('n', '<leader>fb', builtin.buffers, { desc = "Buffer selector"})
+kmap.set('n', '<leader>fh', builtin.help_tags, { desc = "Help tags"})
 
 -- NvimTree
 kmap.set('n', "<leader>fe", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree"} )
@@ -93,8 +116,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 -- Nvim Comment
-kmap.set("n", "<leader>/", "gcc", { desc = "Toggle single line comment" })
-kmap.set("v", "<leader>/", "gc", { desc = "Toggle single line comment" })
+kmap.set("n", "<leader>/", "<Plug>(comment_toggle_linewise_current)", { desc = "Toggle single line comment" })
+kmap.set("v", "<leader>/", "<Plug>(comment_toggle_linewise_visual)", { desc = "Toggle line comment (visual)" })
+-- '<Plug>(comment_toggle_linewise_current)'
+-- or '<Plug>(comment_toggle_linewise_count)'
+
+-- Undotree
+kmap.set("n", "<leader>ut", "<cmd>UndotreeToggle<CR>", { desc = "Toggle Undotree"})
 
 -- Hop
 kmap.set("n", "<leader>hl", "<cmd>HopLineStart<CR>", { desc = "Hop to chosen line" })
@@ -104,3 +132,53 @@ kmap.set("n", "<leader>hW", "<cmd>HopWordMW<CR>", { desc = "Hop to chosen word a
 kmap.set("n", "<leader>hc", "<cmd>HopChar2<CR>", { desc = "Hop to chosen character" })
 kmap.set("n", "<leader>hC", "<cmd>HopChar2MW<CR>", { desc = "Hop to chosen character across buffers" })
 kmap.set("n", "<leader>hh", "<cmd>HopChar1CurrentLine<CR>", { desc = "Hop to chosen character in current line" })
+
+-- nvim Comp
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local cmp = require('cmp')
+local luasnip = require("luasnip")
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- `Tab` key to select option
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+        -- they way you will only jump inside the snippet region
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+  }
+})
