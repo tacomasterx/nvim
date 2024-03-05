e0714c73448048e9d94dd6b03512e4206f49e15a
Tomas Carrillo <tacoman8918@gmail.com>
2024-03-05 01:26:48 -0600

    Just messing a little with lua functions.

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -12,27 +12,27 @@ end
 
 M.change_color_schemes = function ()
     if vim.fn.search("--clr-slate400") ~= 0 then
-       vim.cmd([[%s/--clr-slate400/--clr-title/g]]) 
+       vim.cmd([[%s/--clr-slate400/--clr-title/g]])
     -- else
     --     print("Err")
     end
     if vim.fn.search("--clr-slate600") ~= 0 then
-       vim.cmd([[%s/--clr-slate600/--clr-main/g]]) 
+       vim.cmd([[%s/--clr-slate600/--clr-main/g]])
     -- else
     --     print("Err")
     end
     if vim.fn.search("--clr-slate800") ~= 0 then
-       vim.cmd([[%s/--clr-slate800/--clr-subtitle/g]]) 
+       vim.cmd([[%s/--clr-slate800/--clr-subtitle/g]])
     -- else
     --     print("Err")
     end
     if vim.fn.search("--clr-rose") ~= 0 then
-       vim.cmd([[%s/--clr-rose/--clr-button/g]]) 
+       vim.cmd([[%s/--clr-rose/--clr-button/g]])
     -- else
     --     print("Err")
     end
     if vim.fn.search("--clr-indigo") ~= 0 then
-       vim.cmd([[%s/--clr-indigo/--clr-strong/g]]) 
+       vim.cmd([[%s/--clr-indigo/--clr-strong/g]])
     -- else
     --     print("Err")
     end
