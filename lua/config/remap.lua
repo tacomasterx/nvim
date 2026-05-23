ebac5e215c443943f8830b991f6a4f4ac4ae4103
Tomas Carrillo <tacoman8918@gmail.com>
2026-05-23 13:24:16 -0500

    Last commit before complete reinstall

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -302,13 +302,13 @@ kmap.set("n", "<leader>zss", function()
 end, {desc = "Toggle spell Spanish"})
 
 -- nvim Comp
+--[[
 local has_words_before = function()
     unpack = unpack or table.unpack
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
---[[
 local cmp = require('cmp')
 local luasnip = require("luasnip")
 local cmp_action = require('lsp-zero').cmp_action()
